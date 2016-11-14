524a63720ee8c61a0f9396422641b30e658f1c6d
B13rg <b11484@gmail.com>
2016-11-13 23:50:29 -0800

    Regular Expression
    Adjusted regular expressions to match the descriptions on the login and
    signup pages.

--- forms.py
+++ forms.py
@@ -4,16 +4,16 @@ from wtforms.validators import Regexp, Length, Email
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[Regexp(r'^[\w_-~]+$'), Length(min=4, max=25)])
-    password = PasswordField('password', validators=[Regexp(r'^[\w_-~]+$'), Length(min=6)])
+    username = StringField('username', validators=[Regexp(r'^[\w-]+$'), Length(min=4, max=25)])
+    password = PasswordField('password', validators=[Length(min=6)])
     remember_me = BooleanField('remember_me', default=False)
     login = SubmitField('login')
 
 
 class SignupForm(FlaskForm):
-    username = StringField('username', validators=[Regexp(r'^[\w_-~]+$'), Length(min=4, max=25)])
+    username = StringField('username', validators=[Regexp(r'^[\w-]+$'), Length(min=4, max=25)])
     password = PasswordField('password', validators=[Length(min=6)])
     email = StringField('email',  validators=[Email('This field requires a valid email address')])
-    collection_name = StringField('collection', validators=[Regexp(r'^[a-zA-Z0-0 ]*$')])
+    collection_name = StringField('collection', validators=[Regexp(r'^[a-zA-Z\d ]*$')])
     remember_me = BooleanField('remember_me', default=False)
     signup = SubmitField('sign up')
