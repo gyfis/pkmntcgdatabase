5609f92bd1b28d69612f39d8115cf4a9a03e62da
Tomas Hromada <gyfis@seznam.cz>
2016-11-15 10:18:32 +0100

    updated create index script

--- scripts/create_indexes.py
+++ scripts/create_indexes.py
@@ -2,12 +2,12 @@ import rethinkdb as r
 
 r.connect('localhost', 28015).repl()
 
-cards = r.db('pkmntcgdb').table('cards')
-sets = r.db('pkmntcgdb').table('card_sets')
-user_cards = r.db('pkmntcgdb').table('user_cards')
-card_set_orderings = r.db('pkmntcgdb').table('card_set_orderings')
+db_name = 'pkmntcgdb'
+cards = r.db(db_name).table('cards')
+sets = r.db(db_name).table('card_sets')
+user_cards = r.db(db_name).table('user_cards')
+card_set_orderings = r.db(db_name).table('card_set_orderings')
 
 sets.index_create('set_name').run()
 user_cards.index_create('card_id').run()
 card_set_orderings.index_create('set_code').run()
-
